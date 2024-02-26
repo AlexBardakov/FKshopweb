a8a41b19de7fc29ef68b590bfa51a15936d9989f
KompukterMl <Battleover@yandex.ru>
2024-02-26 22:03:30 +0700

    Создал проект, споткнулся на редактировании HTML файла главной странички

---
+++ myshop/shop/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.SET_DEFAULT, default='Наши сыры',
+                                 verbose_name='Категория')
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, db_index=True, verbose_name='Ссылка')
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name='Изображение')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    price = models.IntegerField(verbose_name='Цена')
+    stock = models.PositiveIntegerField(verbose_name='Количество')
+    available = models.BooleanField(default=True, verbose_name='Доступно для заказа')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    class Meta:
+        ordering = ('name',)
+        index_together = [['id', 'slug'],]
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
